723dc103f391fe634362a1f91ac0bb4e5a7a775a
LoopBreaks <113218220+LoopBreaks@users.noreply.github.com>
2022-09-14 15:32:54 +0530

    Add files via upload

---
+++ variables2.py
@@ -0,0 +1,45 @@
+# x = "Hello Itz me"
+# y = 'Hello Itz Me'
+# print(x)
+# print(y)
+
+# Camel case
+myFruitBasket = "MAngo, Orange"
+# print(myFruitBasket)
+#  pascal Case
+MyFruitBasket = "MAngo, Orange2"
+# print(MyFruitBasket)
+# Snake Case 
+my_fruit_basket = "MAngo, Orange3"
+# print(my_fruit_basket)
+
+FRUIT = "Capital Fruit"
+# print(FRUIT)
+fruit = "Small fruit"
+# print(fruit)
+fruit = "last fruit var"
+# print(fruit)
+
+_my_fruit = "Banana"
+# print(_my_fruit)
+
+
+# 2fruit = ""
+# print(2fruit)
+# my-fruit = ""
+
+# my fruit = ""
+
+x = y = z = "Apple"
+# print(x)
+# print(y)
+# print(z)
+
+x, y, z = "Apple", "Orange", "Banana"
+print(x)
+print(y)
+print(z)
+
+
+
+
